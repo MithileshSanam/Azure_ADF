9ea3eb5fd1974aa8029a8de1006e4e8d871886cd
Mithilesh Sanam <mithilesh.sanam@gmail.com>
2025-02-20 00:54:45 +0530

    Update ddl.sql

--- DE_Project/SQL_Scripts/ddl.sql
+++ DE_Project/SQL_Scripts/ddl.sql
@@ -12,3 +12,32 @@ create table [dbo].[Entity](
 entityID INT Primary Key Identity(1,1),
 entityName Varchar(50)
 );
+
+
+CREATE TABLE [dbo].[Arancione] (
+    ArancioneID INT PRIMARY KEY IDENTITY(1,1),      
+    OnlineRetailer VARCHAR(255),           
+    SalesMonth DATE,                       
+    Title VARCHAR(255),                  
+    Vintage INT,                          
+    Variety VARCHAR(100),                 
+    Score INT,                            
+    ListPrice DECIMAL(10, 2),               
+    Quantity INT                             
+);
+
+
+CREATE TABLE [dbo].[Celeste] (
+    TransactionId UNIQUEIDENTIFIER PRIMARY KEY, 
+    TransactionDate DATE,                       
+    OnlineRetailer VARCHAR(255),               
+    SalesMonth DATE,                            
+    SalesRegion VARCHAR(100),                   
+    SalesCurrency VARCHAR(10),                 
+    Title VARCHAR(255),                         
+    Vintage INT,                                
+    Variety VARCHAR(100),                       
+    Score INT,                                  
+    ListPrice DECIMAL(10, 2),                  
+    Quantity INT                           
+);
