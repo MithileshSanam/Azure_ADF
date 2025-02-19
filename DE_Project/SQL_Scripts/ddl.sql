e3a8848f57cdb76af316e77b0c706061f06c1991
Mithilesh Sanam <mithilesh.sanam@gmail.com>
2025-02-20 00:07:43 +0530

    Update ddl.sql

--- DE_Project/SQL_Scripts/ddl.sql
+++ DE_Project/SQL_Scripts/ddl.sql
@@ -3,5 +3,12 @@ entityID INT,
 loadStartTime DATETIME,
 status VARCHAR(50),
 loadEndTime DATETIME,
+pipelineID VARCHAR(50),
 processID INT Primary Key Identity(1,1)
+);
+
+
+create table [dbo].[Entity](
+entityID INT Primary Key Identity(1,1),
+entityName Varchar(50)
 );
