6a160a5f101214a7579869b2a040e6a3cc082a52
Mithilesh Sanam <mithilesh.sanam@gmail.com>
2025-02-20 20:51:10 +0530

    Create dml.sql

---
+++ DE_Project/SQL_Scripts/dml.sql
@@ -0,0 +1,8 @@
+insert into [dbo].[Celeste] 
+(Transactionid, TransactionDate, OnlineRetailer, SalesMonth, SalesRegion, SalesCurrency, Title, Vintage, Variety, Score, ListPrice, Quantity) 
+values 
+('27fe7a49-433b-4740-a3e4-000e8e021545', '2022-02-11', 'Celeste', '2022-02-01', 'UK', 'GBP', 'Antonino Tringali-Casanuova 2005 Patrimonio Sangiovese (Toscana)', 2005, 'Sangiovese', 86, 15.00, 2),
+('b3a117f1-6e2f-4b57-935e-82b97d8d6de3', '2023-03-02', 'Celeste', '2023-03-01', 'USA', 'USD', 'Solis Estate 2017 Cabernet Sauvignon', 2017, 'Cabernet Sauvignon', 91, 20.50, 3),
+('a5920e1b-9f12-05bc-83a9-8ec6fd9a9677', '2025-10-08', 'Celeste', '2021-11-01', 'Australia', 'AUD', 'Woodlands 2016 Chardonnay', 2016, 'Chardonnay', 89, 18.75, 1);
+
+
